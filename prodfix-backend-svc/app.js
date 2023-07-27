6da9748ed6a3cf0d424c5ff4153b8c0670f9f38c
rahulrajanmhambrey <rahul.mhambrey@whatfix.com>
2023-07-27 19:44:03 +0530

    mongo calls

--- prodfix-backend-svc/app.js
+++ prodfix-backend-svc/app.js
@@ -1,6 +1,13 @@
+const taskService = require("./tasksvc.js");
+
 const express = require("express");
 
 const app = express();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017";
+
+
 app.use(express.json());
 
 app.post('/prodfix/login/', function (req, res) {
@@ -8,12 +15,48 @@ app.post('/prodfix/login/', function (req, res) {
  });
 
  app.post('/prodfix/task/add', function (req, res) {
-    res.send(req.body.task_description);
+
+   var task = {
+      taskname: req.body.task_name,
+      taskdescription: req.body.task_description,
+      url: req.body.url,
+      baseurl: req.body.base_url,
+      elementdetails: req.body.element_details,
+      createdtimestamp: new Date().toLocaleString()
+   };
+
+   console.log(task);
+
+   AddTask(task).then(result => {
+    res.send(result);
+   });
+    
  });
 
  app.get('/prodfix/tasks/', function (req, res) {
     res.send('Get tasks');
  });
+
+ async function AddTask(taskdetails) {
+   var result = false;
+   var client = new MongoClient(url);
+
+   var task = { _taskname: taskdetails.taskname,
+    _taskdescription: taskdetails.taskdescription, _url: taskdetails.url, _baseurl: taskdetails.baseurl,
+     _elementdetails: taskdetails.elementdetails, _createdtimestamp: taskdetails.createdtimestamp };
+
+   await client.db("prodfixdb").collection("tasks").insertOne(task).then((res) => {
+    console.log(res);
+    result = true;
+    client.close();
+   });
+
+     return result;
+}
+
+async function GetTasks(){
+  var client = new MongoClient(url);
+}
 
  var server = app.listen(8000, function () {
     var host = server.address().address
